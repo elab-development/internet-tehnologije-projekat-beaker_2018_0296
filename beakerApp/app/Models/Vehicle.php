d9d241dfe8b0c1f852420ceb9e29531e64af54d1
Stefan <sm20180296@student.fon.bg.ac.rs>
2024-01-04 16:52:02 +0100

    Napravljena 3 modela

--- beakerApp/app/Models/Vehicle.php
+++ beakerApp/app/Models/Vehicle.php
@@ -5,8 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class User extends Model
+class Vehicle extends Model
 {
     use HasFactory;
-    protected $fillable=['name','email','password'];
+
+    protected $fillable=['manufacturer','model','year','capacity','rental price'];
 }
