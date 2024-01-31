029b59d7e3e9817eeff09e61d9e5408b53f34e0f
Stefan <sm20180296@student.fon.bg.ac.rs>
2024-01-31 17:34:43 +0100

    Inicijalizovani atirbuti klase StanjeAukcije na pocetne vrednosti

--- Aukcija/backend/smart_contracts/aukcija/contract.py
+++ Aukcija/backend/smart_contracts/aukcija/contract.py
@@ -2,25 +2,29 @@ import beaker
 import pyteal as pt
 from algokit_utils import DELETABLE_TEMPLATE_NAME, UPDATABLE_TEMPLATE_NAME
 
-app = beaker.Application("aukcija")
 
+class StanjeAukcije:
+    pass
 
-@app.update(authorize=beaker.Authorize.only_creator(), bare=True)
-def update() -> pt.Expr:
-    return pt.Assert(
-        pt.Tmpl.Int(UPDATABLE_TEMPLATE_NAME),
-        comment="Check app is updatable",
+    prethodnji_ponudjac = beaker.GlobalStateValue(
+        stack_type=pt.TealType.bytes, default=pt.Bytes("")
     )
-
-
-@app.delete(authorize=beaker.Authorize.only_creator(), bare=True)
-def delete() -> pt.Expr:
-    return pt.Assert(
-        pt.Tmpl.Int(DELETABLE_TEMPLATE_NAME),
-        comment="Check app is deletable",
+    prethodna_ponuda = beaker.GlobalStateValue(
+        stack_type=pt.TealType.uint64, default=pt.Int(0)
     )
+    kraj_aukcije = beaker.GlobalStateValue(
+        stack_type=pt.TealType.uint64, default=pt.Int(0)
+    )
+    token_id = beaker.GlobalStateValue(stack_type=pt.TealType.uint64, default=pt.Int(0))
+    trazeni_iznos = beaker.LocalStateValue(
+        stack_type=pt.TealType.uint64, default=pt.Int(0)
+    )
+    token = beaker.GlobalStateValue(stack_type=pt.TealType.uint64, default=pt.Int(0))
 
 
-@app.external
-def hello(name: pt.abi.String, *, output: pt.abi.String) -> pt.Expr:
-    return output.set(pt.Concat(pt.Bytes("Hello, "), name.get()))
+app = beaker.Application("aukcija", state=StanjeAukcije)
+
+
+@app.create(bare=True)
+def create() -> pt.Expr:
+    return app.initialize_global_state()
